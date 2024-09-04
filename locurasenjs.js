945710e6106f3b75d134b97e943e146fcce167e2
donomargv <fangaseltangas@gmail.com>
2024-09-03 19:37:27 -0500

    first commit

---
+++ locurasenjs.js
@@ -0,0 +1,122 @@
+
+// //tipo de datos
+// "hola menor" //string
+// 'hola menor de nuevo' //string
+
+// //numeros
+// 1200000
+// 10000.069
+// -4.5
+// //boleanos 
+
+// true / falce 
+
+// //items o array
+// ['jouu jouu', 'jorgisiuspv','elnr brayan pv']
+// [1,2,2,3,4,5,6,78,9,0]
+// [true,falce,true,falce]
+
+// //objetos object
+// 'omar'
+// 100
+// 14
+// true
+
+// {
+//     "username ": 'omar',
+//     "score": 100,
+//     "hours": 14,
+//     "profecional": true,
+// }
+// {
+//     "username ": 'jorgi',
+//     "score": 20,
+//     "hours": 40,
+//     "profecional": falce,
+// // }
+// console.log('esto es una cadena')
+// console.log({"username"=omar pv})
+
+// // variables
+// var nameuser = 'pepe el loco'
+// let lastname ='de el puente'
+// nameuser ='pepe '
+// const pi =3.1416;
+// console.log(lastname)
+
+// //camelcase(comentario no se nota ni da error )
+// let nickname ='perrosvigrajosos';
+// let numerouno ='omar';
+// let numerodos='garcia';
+// let complete =numerouno + numerodos
+// let resultado =numeropv > numerogv
+// let numeropv =100
+// let numerogv =500
+// let contraseña ='pepepv'
+// let result = imput ==contrasmneor 
+
+// if(resultado=== true)
+// {
+//     console.log(login$$$)
+// }
+// else
+// {
+//     console.log('login poderoso menor')
+// }
+// if (resultado === falce)
+// {
+//     console.log(loginXXX)
+// // }
+// // console.log(resultado)
+// let score =25 ;
+// if (score > 30){
+//     console.log('nesesita mas practicas menor')
+
+// }
+// else if (score >15 ){
+//     console.log('esta mejor estoy orgulloso menor')
+// }
+
+
+// else {
+//     console.log('nesesita seguir a momasos diego')
+// }
+// let targeta ='targeta '
+// switch (targeta ){
+//     case 'targeta'
+//     console.log('su targeta si  es targeta ')
+//     break;
+//     case 'targeta pv'
+//     console.log('nada de las nadas menor sins')
+//     break;
+
+// // }
+// //bucles
+// for (let i = 0; i < 50; i++) {
+//     console.log(`Hola menor de nuevo aqui jajajjajaj ${i}`);
+// }
+// const usuarios = ['Juan', 'María', 'Carlos', 'Ana'];
+
+// for (const usuario of usuarios) {
+//     console.log(`Hola, ${usuario}`);
+// }
+//funciones 
+// function saludar(nombre)
+// {
+//     console.log('hola'+nombre)
+
+
+// }
+// saludar ('jorgy');
+// saludar ('nanay');
+// saludar ('locuras');
+// saludar ('en el');
+// saludar ('basque');
+function sumar(n1, n2){
+    console.log(n1 +n2)
+}
+sumar (2,1)
+sumar (99,1)
+sumar (34,1)
+sumar (45,1)
+sumar (2,1)
